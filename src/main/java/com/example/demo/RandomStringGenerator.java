e686b956ab046819a0c9bdbc344aaa9163a0e947
genkichi <genshigoto0821@gmail.com>
2025-08-06 14:20:04 +0900

    RandomStringGenerator.java  コメント追加

--- src/main/java/com/example/demo/RandomStringGenerator.java
+++ src/main/java/com/example/demo/RandomStringGenerator.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
-//処理を書く場所だとSpringに伝えるアノテーション。他のクラスから自動で使えるようになる
+//処理を書く場所だとSpringに伝えるアノテーション。他のクラスから自動で使えるようになる　(Model)
 @Service
 public class RandomStringGenerator {
 //	フィールド定数
